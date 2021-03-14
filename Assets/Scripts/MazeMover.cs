5c06901826cb6b886cb3fc5ba00fce0ab0940c82
Mickael Suisse <m.suisse@gmail.com>
2021-03-14 03:50:09 +0100

    First commit to test

--- Assets/Scripts/MazeMover.cs
+++ Assets/Scripts/MazeMover.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MazeMover : MonoBehaviour
 {
+
+    float velocity = 0.3f;
+    Vector2 direction = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction.x = 1 * velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate((Vector3)direction);
         
     }
 }
